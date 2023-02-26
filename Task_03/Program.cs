4981658aa19928fd3ba378438f972ba2ab67bfc9
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-26 13:27:36 +0300

    task_03 corr2 added

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,44 +2,50 @@
 //и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-int[] MyArray(int length, int minValue, int maxValue)
+int [] MyArray(int size)
 {
-    int[] result = new int[length];
-    Random rnd = new Random();
-    for (int i = 0; i < result.Length; i++)
+    int [] myArray = new int[size];
+    for(int i = 0; i < myArray.Length; i++)
     {
-        result[i] = rnd.Next(minValue, maxValue);
+        myArray[i] = new Random().Next();
     }
-    return result;
+    return myArray;
 }
 
-void PrintArray(int[] array)
+void PrintArray(int[] myArray)
 {
-    for (int i = 0; i < array.Length; i++)
+    for(int i = 0; i < myArray.Length; i++)
     {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
-
-int[] DegreeNumber(int[] array)
-{
-    double maxNumber = array[0];
-    double minNumber = array[0];
-    degreeNumber = maxNumber - minNumber;
-    {
-        for (int i = 1; i < array.Length; i++)
-    {
-        if (array[i] > maxNumber) maxNumber = array[i];
-        if (array[i] < minNumber) minNumber = array[i];
-    }
-    return degreeNumber;
+        Console.Write($" {myArray[i]} ");
     }
 }
 
-int size = 10;
-int minValue = 1;
-int maxValue = 50;
-int[] myArray = MyArray(size, minValue, maxValue);
+int IntputSizeArray(string msg)
+{
+    Console.WriteLine($" {msg} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int DegreeNumber(int[] array)
+{
+    int maxNumber = array[0];
+    int minNumber = array[1];
+    if(maxNumber < minNumber)
+    {
+        maxNumber = minNumber;
+        minNumber = array[0];
+    }
+    for (int i = 2; i < array.Length; i++)
+        {
+            if (array[i] > maxNumber) maxNumber = array[i];
+            if (array[i] < minNumber) minNumber = array[i];
+        }
+    return  Math.Round(maxNumber - minNumber,2); 
+}
+
+int sizeArray = IntputSizeArray("Введите размер массива ");
+int [] myArray = MyArray(sizeArray);
 PrintArray(myArray);
+
+
 Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {DegreeNumber(myArray)}");
