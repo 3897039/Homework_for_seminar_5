952313ce22797c0d08de7eb6951ef508408c43a2
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-26 12:39:33 +0300

    task_03 corr1 added

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,10 +2,10 @@
 //и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-int[] MyArray(int length,int minValue, int maxValue)
+int[] MyArray(int length, int minValue, int maxValue)
 {
     int[] result = new int[length];
-    Random rnd = new Random(); 
+    Random rnd = new Random();
     for (int i = 0; i < result.Length; i++)
     {
         result[i] = rnd.Next(minValue, maxValue);
@@ -15,42 +15,31 @@ int[] MyArray(int length,int minValue, int maxValue)
 
 void PrintArray(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
     }
     Console.WriteLine();
 }
 
-int DiffNumber(int[] array)
+int[] DegreeNumber(int[] array)
 {
-    int maxNumber = 0;
-    int minNumber = 0;
-    int diffnumber = 0;
-        for(int i = 0; i < array.Length; i++)
-        {
-            {
-                {
-                    if(maxNumber < array[i]) maxNumber = array[i];
-                }
-                return maxNumber;
-                 {
-                    if(minNumber < array[i]) minNumber = array[i];
-                 }
-                return minNumber;
-            }
-            diffnumber = maxNumber - minNumber;
-        } 
-        return diffnumber;     
+    double maxNumber = array[0];
+    double minNumber = array[0];
+    degreeNumber = maxNumber - minNumber;
+    {
+        for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > maxNumber) maxNumber = array[i];
+        if (array[i] < minNumber) minNumber = array[i];
+    }
+    return degreeNumber;
+    }
 }
 
 int size = 10;
 int minValue = 1;
-int maxValue = 200;
+int maxValue = 50;
 int[] myArray = MyArray(size, minValue, maxValue);
 PrintArray(myArray);
-Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {DiffNumber(myArray)}");
-
-
-
-
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {DegreeNumber(myArray)}");
